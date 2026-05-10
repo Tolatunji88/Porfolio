31944f53aeffed4cb0603ca47d046910e729b799
Tolatunji88 <tolu.r.olatunji@gmail.com>
2026-05-10 14:26:26 -0400

    Refactor title extraction and update styles in ImageResponse
    Refactor URL parameter extraction and enhance styling.

--- src/app/api/og/generate/route.tsx
+++ src/app/api/og/generate/route.tsx
@@ -3,13 +3,22 @@ import { ImageResponse } from 'next/og';
 import { baseURL, person } from "@/resources";
 
 export async function GET(request: Request) {
-    let url = new URL(request.url);
-    const title = url.searchParams.get('title') || person.name;
+    const { searchParams } = new URL(request.url);
+    const title = searchParams.get('title') || person.name;
 
     return new ImageResponse(
         (
-            <div style={{ display: 'flex', background: 'black', width: '100%', height: '100%', color: 'white', alignItems: 'center', justifyContent: 'center' }}>
-                <h1>{title}</h1>
+            <div style={{ 
+                display: 'flex', 
+                background: 'black', 
+                width: '100%', 
+                height: '100%', 
+                color: 'white', 
+                alignItems: 'center', 
+                justifyContent: 'center',
+                fontSize: '64px'
+            }}>
+                {title}
             </div>
         ),
         { width: 1200, height: 630 }
